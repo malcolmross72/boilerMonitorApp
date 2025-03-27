026e1ffa331bebb08c16d0ccf01aad2c78c3a4d4
malcolmross72 <mross5766763@mysundance.ca>
2025-03-27 02:59:26 -0600

    Initial commit: Boiler Monitor App with HTLM, SCC, and JS skeleton

---
+++ CSS/Scripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    console.log("Boiler Monitor App is loaded.");
+
+    // Placeholder for future features
+    // Example: Password checking, data storage, graph drawing, suggestions.....
+});
